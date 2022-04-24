176ed5174ebc41dcad294bcafd6532ec69398635
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-24 19:46:52 -0400

    select element

---
+++ select.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    var_dump($_POST);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forms</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<form method="post">
+
+    <select name="country">
+        <option value="germany">Germany</option>
+        <option value="france">France</option>
+        <option value="uk" selected>United Kingdom</option>
+        <option value="brazil">Brazil</option>
+        <option value="canada">Canada</option>
+        <option value="usa">United States</option>
+    </select>
+
+    <button>Send</button>
+
+</form>
+
+</body>
+</html>
