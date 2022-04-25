889560fe5b211211f5bc7860b7c8e5bc78966010
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-25 19:58:37 -0400

    form validation and display error messages

--- new-article.php
+++ new-article.php
@@ -3,26 +3,39 @@
 ini_set('display_errors', 1); 
 require 'includes/database-connection.php'; 
 
+$errors = [];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $connection = getDB();
-    $sql = "INSERT INTO article (title, content, published_at) 
-            VALUES (?,?,?)";                  
-    try {
-        $sqlStmt = mysqli_prepare($connection, $sql); 
-        if ($sqlStmt) {
-            mysqli_stmt_bind_param($sqlStmt, 'sss', $_POST['title'], $_POST['content'], $_POST['published_at']);
-            if (mysqli_stmt_execute($sqlStmt)) {
-                $id = mysqli_stmt_insert_id($sqlStmt);
-                echo "Inserted Record was at ID: {$id}";
-            } else {
-                echo mysqli_stmt_error($sqlStmt);
-            }
-        } else {
-            echo "DB did not return a value: " . mysqli_error($connection); 
-        }
-    } catch (Exception $e) {
-        echo "ERROR: " . $e; 
+
+    if ($_POST['title'] == '') {
+        $errors[] = "Title should not be empty!";
     }
+    if ($_POST['content'] == '') {
+        $errors[] = "Content should not be empty!";
+    }
+    
+    if (empty($errors)) {
+        $connection = getDB();
+        $sql = "INSERT INTO article (title, content, published_at) 
+                VALUES (?,?,?)";                  
+        try {
+            $sqlStmt = mysqli_prepare($connection, $sql); 
+            if ($sqlStmt) {
+                mysqli_stmt_bind_param($sqlStmt, 'sss', $_POST['title'], $_POST['content'], $_POST['published_at']);
+                if (mysqli_stmt_execute($sqlStmt)) {
+                    $id = mysqli_stmt_insert_id($sqlStmt);
+                    echo "Inserted Record was at ID: {$id}";
+                } else {
+                    echo mysqli_stmt_error($sqlStmt);
+                }
+            } else {
+                echo "DB did not return a value: " . mysqli_error($connection); 
+            }
+        } catch (Exception $e) {
+            echo "ERROR: " . $e; 
+        }
+    }
+
 }
 
 ?>
@@ -32,6 +45,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <h2>New article</h2>
 
 <form method="post">
+
+    <ul>
+        <?php foreach ($errors as $err) : ?>
+            <li> <?= $err ?>
+        <?php endforeach ?>
+    </ul>
 
     <div>
         <label for="title">Title</label>
