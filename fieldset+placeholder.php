ac512bc39088ed97b37380e1cacb252b153d2250
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-24 20:42:57 -0400

    use fieldset with legend and placeholders

---
+++ fieldset+placeholder.php
@@ -0,0 +1,49 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    var_dump($_POST);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forms</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<form method="post">
+    <fieldset>
+        <legend>Article</legend>
+        <div>
+            <label for="title">Title:</label> <input type="text" name="title" id="title" placeholder="Name">
+        </div>
+
+        <div>
+            <label for="content">Content:</label> <textarea name="content" rows="4" cols="40" id="content" placeholder="Description"></textarea>
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Attributes</legend>
+        <div>
+            <label><input type="checkbox" name="visible" value="yes">Visible</label>
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Colour</legend>
+        <div>
+            <label><input type="radio" name="colour" value="blue" checked>Blue<br></label>
+            <label><input type="radio" name="colour" value="red">Red<br></label>
+            <label><input type="radio" name="colour" value="green">Green</label>
+        </div>
+    </fieldset>
+    <button>Send</button>
+
+</form>
+
+</body>
+</html>
